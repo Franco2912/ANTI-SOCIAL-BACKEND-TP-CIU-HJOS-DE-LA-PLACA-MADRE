e2101cf78993948a90d93df6f9c1332cefe71b83
Nicolas389 <nicolasjgonzalez389@gmail.com>
2026-06-24 14:48:03 -0300

    se agrega dependencia redis y su configuracion

--- src/main.js
+++ src/main.js
@@ -4,14 +4,15 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-
 const postRoutes = require('./routes/router.post');
 const userRoutes = require('./routes/router.user');
 const commentRoutes = require('./routes/router.comment');
+const redisClient = require('./services/redis.service');
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/unahur-red-social';
+const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors());
 app.use(express.json());
@@ -24,19 +25,26 @@ app.use((req, res) => {
     res.status(404).json({ error: 'Ruta no encontrada' });
 });
 
-// 5. Conexión a MongoDB y arranque del servidor HTTP
-mongoose.connect(MONGO_URI)
-    .then(() => {
-        console.log('Conexión exitosa a MongoDB con Mongoose.');
-        
-        // Levanta el servidor Express SÓLO si la base de datos conectó correctamente
-        app.listen(PORT, () => {
-            console.log(`Servidor corriendo en http://localhost:${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.error('Error crítico: No se pudo conectar a MongoDB:', error.message);
-        process.exit(1); 
-    });
+
+async function start() {
+    try{
+        // Conexion a MongoDB
+        await mongoose.connect(MONGO_URI)
+        console.log('Conexión exitosa a MongoDB con Mongoose.')
+
+       // Conexion a redis
+       await redisClient.connect()
+       console.log('Conexion exitosa a Redis.')
+
+       app.listen(PORT, () => {
+        console.log(`Servidor corriendo en http://localhost:${PORT}`)
+       })
+    }catch(error){
+        console.error('Error al iniciar la aplicacion', error)
+        process.exit(1)
+    }
+}
+
+start();
 
 module.exports = app;
---
+++ src/services/redis.service.js
@@ -0,0 +1,12 @@
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URL ?? "redis://localhost:6379",
+  password: process.env.REDIS_PASSWORD,
+  connectTimeout: 10000,
+});
+
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+redisClient.on("connect", () => console.log("Redis Client connecto OK"));
+
+module.exports = redisClient;
